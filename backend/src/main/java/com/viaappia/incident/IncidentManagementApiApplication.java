e8afbcac1485f6301580c81af884bb2a57182db1
Filipe <filipelopessant028@gmail.com>
2026-07-09 16:56:29 -0300

    chore: initialize incident management system backend infrastructure

---
+++ backend/src/main/java/com/viaappia/incident/IncidentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.viaappia.incident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentManagementApiApplication.class, args);
+	}
+
+}
